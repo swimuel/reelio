cb3645901ed54fae8ceaca09988ec76d31701649
Darcy Cox <darcycox97@gmail.com>
2019-08-14 22:41:35 +1200

    Remove hello world endpoints

--- server/server.js
+++ server/server.js
@@ -10,17 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
 // connect to database
 const dbUrl = 'mongodb://localhost/reelio'
 mongoose.connect(dbUrl, { useNewUrlParser: true, useCreateIndex: true })
