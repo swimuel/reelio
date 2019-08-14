0c21011182a315a1d58a59e7f3219c6212596235
Darcy Cox <darcycox97@gmail.com>
2019-08-14 22:19:39 +1200

    Add example code for api-related stuff

---
+++ server/api/example/example.js
@@ -0,0 +1,14 @@
+// this is a file defining the database schema for the given entity. 
+// i.e. this defines the "example" entity, which can be interacted with via the
+// api/example endpoint (as configured in the routes file).
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// see https://mongoosejs.com/docs/guide.html#definition for how to create schemas
+const ExampleSchema = new Schema({
+    title: String,
+    content: String
+})
+
+module.exports = mongoose.model('Example', ExampleSchema)
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const glob = require('glob');
+const mongoose = require('mongoose')
+const Example = require('./api/example/example')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+// connect to database
+const dbUrl = 'mongodb://localhost/reelio'
+mongoose.connect(dbUrl, { useNewUrlParser: true, useCreateIndex: true })
+.then(async () => {
+  console.log(`mongo connected at ${dbUrl}`)
+  // clear database.
+  // add all models to be cleared here
+  await Promise.all([
+    Example.deleteMany({})
+  ]);
+
+  // init data
+  // require all dummy data here and init using insertMany
+  const examples = require('./api/example/example.dummy')
+  try {
+    await Example.insertMany(examples)
+  } catch (e) {
+    console.log(`error initializing data: ${e}`)
+  }
+
+  // REST API routes
+  // configures all REST API routes automatically
+  // const rootPath = require('path').join(__dirname, '/..')
+  glob.sync(__dirname + '/api/**/*.routes.js').forEach(controllerPath => {
+    if (!controllerPath.includes('.spec.js')) require(controllerPath)(app)
+  })
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+})
