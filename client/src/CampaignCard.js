2529376a1c8f06d372c14faf46bac561ea99cf7b
Jess-Alcantara <jessica.alcantara97@gmail.com>
2019-08-26 10:54:32 +1200

    Change time remaining component to use props

--- client/src/CampaignCard.js
+++ client/src/CampaignCard.js
@@ -10,7 +10,7 @@ class CampaignCard extends React.Component {
         hoverable
         style={{ width: 200, height: 440 }}
         cover={<img alt='example' src='http://www.impawards.com/2019/posters/avengers_endgame_ver2_xlg.jpg' width='200' />}
-        extra={<TimeRemaining />}
+        extra={<TimeRemaining time='2 days' />}
       >
         <Meta title='Title of Movie' description='' />
         <Progress percent={75} />
--- client/src/components/CampaignCard/TimeRemaining.js
+++ client/src/components/CampaignCard/TimeRemaining.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import { Tooltip, Icon } from 'antd'
-import { getTime } from '../../api'
 
 class TimeRemaining extends React.Component {
-  state = {
-    time: null
-  }
-
-  async componentDidMount () {
-    const time = await getTime()
-    this.setState({ time: time })
-  }
-
   render () {
     return (
       <div>
-        <Tooltip placement='right' title={this.state.time + ' left'}>
+        <Tooltip placement='right' title={this.props.time + ' left'}>
           <Icon type='clock-circle' />
         </Tooltip>
       </div>
