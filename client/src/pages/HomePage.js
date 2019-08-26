ba8ad72be17c57981b5a6136daa16f8050419175
Cyrus <cyrus.raitava@gmail.com>
2019-08-27 01:20:45 +1200

    Refactor homepage to spawn CampaignCards with data from db

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,26 +2,25 @@ import React from 'react'
 import Header from '../components/home/Header'
 import CampaignCard from '../CampaignCard'
 import CampaignList from '../components/home/CampaignList'
+import { getCampaigns } from '../api'
 
 class HomePage extends React.Component {
-  render () {
-    var campaigns = []
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
-    campaigns.push(new CampaignCard())
+  state = {
+    loading: true,
+    campaigns: null
+  }
 
-    return (
-      <div className={'HomePage'}>
+  async componentDidMount () {
+    const campaigns = await getCampaigns()
+    this.setState({ campaigns: campaigns, loading: false })
+  }
+
+  render () {
+    return this.state.loading ? <p>TODO: loading indicator</p>
+      : <div className={'HomePage'}>
         <Header />
-        <CampaignList campaigns={campaigns} />
+        <CampaignList campaigns={this.state.campaigns.map(e => <CampaignCard props={e} />)} />
       </div>
-    )
   }
 }
 
