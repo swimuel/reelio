baa035c88177cb7718df1bfef52450f733284521
Sam Broadhead <sbroadhd@gmail.com>
2019-08-27 17:01:38 +1200

    refactor card to accomodate new components

--- client/src/components/campaign/CampaignCard.js
+++ client/src/components/campaign/CampaignCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Progress } from 'antd'
+import { Card, Progress, Typography } from 'antd'
 import TimeRemaining from './TimeRemaining'
 const moment = require('moment')
 
@@ -26,21 +26,67 @@ class CampaignCard extends React.Component {
   }
 
   render () {
-    const { Meta } = Card
+    const { Paragraph, Title } = Typography
     const { campaign } = this.props
     return (
       <Card
         hoverable
-        style={{ width: 200, height: 440 }}
-        // TODO: include image url in dto
-        cover={<img alt='campaign poster' src={campaign.imageUrl} width='200' />}
-        extra={<TimeRemaining time={this.calculateTimeRemaining()} />}
+        style={{ ...styles.card, ...{ backgroundImage: `url(${campaign.imageUrl})` } }}
+        bodyStyle={styles.bodyOverride}
       >
-        <Meta title={campaign.filmTitle} description={' (' + campaign.screenType + ')'} />
-        <Progress percent={campaign.percentageComplete} />
-      </Card>
+        <div style={styles.screenType}>
+          <Paragraph style={{ margin: 0 }}>{campaign.screenType}</Paragraph>
+        </div>
+
+        <div style={styles.timeRemaining}>
+          <TimeRemaining time={this.calculateTimeRemaining()} />
+        </div>
+
+        <div style={styles.subCard}>
+          <Title level={4}>{campaign.filmTitle}</Title>
+          <Paragraph>{campaign.campaignTitle}</Paragraph>
+          <Progress percent={campaign.percentageComplete} />
+        </div>
+      </Card >
     )
   }
 }
 
 export default CampaignCard
+
+const styles = {
+  card: {
+    width: 300,
+    height: 440,
+    borderRadius: 5,
+    backgroundSize: 'cover'
+  },
+  bodyOverride: { // doing this allows us to treat the body of the card as the entire card
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    padding: 0
+  },
+  subCard: {
+    position: 'absolute',
+    bottom: 15,
+    marginLeft: '5%',
+    width: '90%',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: '18px'
+  },
+  screenType: {
+    position: 'absolute',
+    left: 20,
+    top: 20,
+    padding: 4,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    borderRadius: 5
+  },
+  timeRemaining: {
+    position: 'absolute',
+    top: 20,
+    right: 20
+  }
+}
