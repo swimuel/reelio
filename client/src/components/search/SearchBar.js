e2ba1a488010c2b78c1852fb7dd740ec71bb168a
Aorthi <aorthi@hotmail.com>
2019-09-04 12:21:12 +1200

    Creating search bar component

---
+++ client/src/components/search/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Input } from 'antd'
+
+class SearchBar extends React.Component {
+
+  render(){
+    const { Search } = Input;
+
+    return (
+      <div>
+      <Search
+    placeholder="search"
+    onSearch={value => console.log(value)}
+    style={{ width: 200 }}
+    />
+    </div>
+    )
+  }
+}
+
+export default SearchBar
