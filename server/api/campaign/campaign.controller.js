16b32370ac52586d716bf0aaf07330b3d48f0e9a
emipeanz <epea390@aucklanduni.ac.nz>
2019-09-06 11:39:16 +1200

    Add basic POST handler for making new campaigns. No error or validation checking yet.

--- server/api/campaign/campaign.controller.js
+++ server/api/campaign/campaign.controller.js
@@ -3,6 +3,7 @@
 // the model can be used to interact with the database entities
 const Campaigns = require('./campaign')
 const ScreenType = require('../screenType/screenType')
+const moment = require('moment')
 
 const getAllCampaigns = async (req, res) => {
   const campaignList = await Campaigns.find()
@@ -27,7 +28,37 @@ const getCampaignById = async (req, res) => {
   res.json(dto)
 }
 
+const newCampaign = async (req, res) => {
+  console.log('new campaign')
+  const campaign = new Campaigns()
+  campaign.filmTitle = req.body.filmTitle
+  campaign.campaignTitle = req.body.campaignTitle
+  campaign.creationDate = moment()
+  campaign.screeningDate = Date.parse(req.body.screeningDate)
+  campaign.screenType = req.body.screenType
+  campaign.imageUrl = req.body.imageUrl
+  campaign.genre = req.body.genre
+  campaign.creatorName = req.body.creatorName
+  campaign.creatorEmail = req.body.creatorEmail
+  campaign.cinemaName = req.body.cinemaName
+  campaign.cinemaAddress = req.body.cinemaAddress
+  campaign.price = req.body.price
+
+  // save the contact and check for errors
+  campaign.save(function (err) {
+    if (err) {
+      res.json(err)
+    } else {
+      res.json({
+        message: 'Done',
+        data: campaign
+      })
+    }
+  })
+}
+
 module.exports = {
   getAllCampaigns,
-  getCampaignById
+  getCampaignById,
+  newCampaign
 }
