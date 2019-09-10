7ffc5e524a265045a732fce63986a0e8dfa031aa
Em Pearce <emipeanz@gmail.com>
2019-09-10 15:28:23 +1200

    Feature/68 endpoint for saving campaigns (#73)
    * Add basic POST handler for making new campaigns. No error or validation checking yet.
    * Add validation for screenTypes and date parsing.
    * Fix response codes not being sent.
    * Condense code by leveraging MongoDB query language.
    * Switch ordering of .json and .status calls

--- server/api/campaign/campaign.routes.js
+++ server/api/campaign/campaign.routes.js
@@ -1,8 +1,9 @@
-const { getAllCampaigns, getCampaignById } = require('./campaign.controller')
+const { getAllCampaigns, getCampaignById, newCampaign } = require('./campaign.controller')
 
 module.exports = (server) => {
   // this function is called for any file ending in .routes.js.
   // use it to configure which controller methods are called for each route
   server.get('/api/campaigns', getAllCampaigns)
   server.get('/api/campaigns/:id', getCampaignById) // :id specifies that id is a route parameter
+  server.post('/api/campaign', newCampaign)
 }
