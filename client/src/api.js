14ed420bea3a73ed740871f2d59c46e1dff949c2
hhagenson28 <holly.hagenson@gmail.com>
2019-09-15 17:08:55 +1200

    Added call to API to retrieve cinemas

--- client/src/api.js
+++ client/src/api.js
@@ -63,3 +63,11 @@ export const getScreenTypes = async () => {
 
   return body
 }
+
+export const getCinemas = async () => {
+  const response = await fetch('/api/cinemas')
+  const body = await response.json()
+  if (response.status !== 200) throw Error(body.message)
+
+  return body
+}
