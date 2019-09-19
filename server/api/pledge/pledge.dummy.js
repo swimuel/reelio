3cb58eefac398088182e893723f8125b55d3e3a0
emipeanz <epea390@aucklanduni.ac.nz>
2019-09-20 09:02:36 +1200

    Update pledge dummy data.

--- server/api/pledge/pledge.dummy.js
+++ server/api/pledge/pledge.dummy.js
@@ -5,24 +5,40 @@ module.exports = [
     name: 'Testy Bro',
     email: 'test@yo.com',
     campaign: campaigns[0]._id,
-    ticketsPledged: 5
+    ticketsPledged: 5,
+    creditCardNumber: '1234567891123456',
+    creditCardExpiry: new Date('2020-12-01'),
+    creditCardCVV: '987',
+    creditCardName: 'That Person'
   },
   {
     name: 'Testy Bro',
     email: 'test@yo.com',
     campaign: campaigns[0]._id,
-    ticketsPledged: 5
+    ticketsPledged: 5,
+    creditCardNumber: '2468101234123456',
+    creditCardExpiry: new Date('2020-05-01'),
+    creditCardCVV: '123',
+    creditCardName: 'Random'
   },
   {
     name: 'Testy Bro',
     email: 'test@yo.com',
     campaign: campaigns[1]._id,
-    ticketsPledged: 9
+    ticketsPledged: 9,
+    creditCardNumber: '13579111212345e61',
+    creditCardExpiry: new Date('2021-02-01'),
+    creditCardCVV: '567',
+    creditCardName: 'Whats his face'
   },
   {
     name: 'Testy Bro',
     email: 'test@yo.com',
     campaign: campaigns[2]._id,
-    ticketsPledged: 1
+    ticketsPledged: 1,
+    creditCardNumber: '1234567876123456',
+    creditCardExpiry: new Date('2021-08-01'),
+    creditCardCVV: '123',
+    creditCardName: 'Person'
   }
 ]
