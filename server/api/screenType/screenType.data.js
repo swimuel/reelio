d8c59bb0ef347fb06c06e0f54546ea04980903ad
emipeanz <epea390@aucklanduni.ac.nz>
2019-09-23 08:18:32 +1200

    Add max number of tickets to screenType schema.

--- server/api/screenType/screenType.data.js
+++ server/api/screenType/screenType.data.js
@@ -5,24 +5,28 @@ module.exports = [
     _id: objectid().toString(),
     name: 'IMAX_3D',
     numTicketsRequired: 50,
-    price: 12
+    price: 12,
+    maxTicketsAvailable: 100
   },
   {
     _id: objectid().toString(),
     name: 'IMAX',
     numTicketsRequired: 50,
-    price: 15
+    price: 15,
+    maxTicketsAvailable: 75
   },
   {
     _id: objectid().toString(),
     name: '2D',
     numTicketsRequired: 20,
-    price: 5
+    price: 5,
+    maxTicketsAvailable: 200
   },
   {
     _id: objectid().toString(),
     name: '3D',
     numTicketsRequired: 30,
-    price: 10
+    price: 10,
+    maxTicketsAvailable: 150
   }
 ]
