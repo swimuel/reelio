2600075d69bfffbe0ceb3855dcc3312117d73f3e
hhagenson28 <holly.hagenson@gmail.com>
2019-09-22 21:11:19 +1200

    Added pledge details and pledge form to pledge page

--- client/src/pages/PledgePage.js
+++ client/src/pages/PledgePage.js
@@ -1,11 +1,84 @@
 import React, { Component } from 'react'
+import { Row, Col, Icon, Card, Spin } from 'antd'
+import { Link } from 'react-router-dom'
+import { getCampaignById } from '../api'
+import PledgeDetails from '../components/pledge/PledgeDetails'
+import PledgeForm from '../components/form/PledgeForm'
+import './NewCampaignPage.css'
 
 class PledgePage extends Component {
-  render () {
-    return (
-      <div />
+  state = {
+    campaign: null,
+    loading: true
+  }
+
+  async componentDidMount() {
+    const id = this.props.match.params.id
+    const campaign = await getCampaignById(id)
+    this.setState({ campaign: campaign, loading: false, CanSubmitForm: false })
+  }
+
+  render() {
+    const { campaign } = this.state
+
+    return this.state.loading ? <Spin /> : (
+      <div>
+        <Row>
+          <Col span={24}>
+            <Card bordered={false}>
+              <Link to={`/campaigns/${campaign._id}`}><Icon type='arrow-left' style={styles.backButton} /></Link>
+              <h1 style={{ fontSize: '2.8em' }}>Pledge to '{campaign.campaignTitle}'</h1>
+              <Icon type='arrow-left' style={styles.backButton2} />
+              <h1>A screening of {campaign.filmTitle}</h1>
+            </Card>
+          </Col>
+        </Row>
+        <div className={'details-container'}>
+          <div className={'custom-col-1'}>
+            <Card bordered={false} style={styles.cardBorder}>
+              <PledgeForm sendDetails={this.fromCampaignPledgeForm} canSubmit={this.state.CanSubmitForm} />
+            </Card>
+          </div>
+          <div className={'custom-col-2'}>
+            <PledgeDetails id={campaign._id} imdbID={campaign.imdbID} />
+          </div>
+        </div>
+      </div>
     )
+  }
+
+  fromPledgeForm = (formData) => {
+    this.setState({ PledgeForm: formData }, () => {
+      this.saveData()
+    })
+  }
+
+  saveData = () => {
+    const pledge = {
+      name: this.state.PledgeForm.name,
+      email: this.state.PledgeForm.email,
+      campaign: this.state.campaign._id,
+      ticketsPledged: this.state.PledgeForm.ticketsPledged
+    }
   }
 }
 
 export default PledgePage
+
+const styles = {
+  backButton: {
+    float: 'left',
+    fontSize: '2em',
+    color: '#FF6852'
+  },
+
+  backButton2: {
+    float: 'left',
+    fontSize: '2em',
+    color: '#FFFFFF'
+  },
+
+  cardBorder: {
+    borderRight: '1px solid #E8E8E8'
+  }
+}
