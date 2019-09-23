594e994171ee9fba3ba8b278837aed9b9b6edcbf
Darcy Cox <darcycox97@gmail.com>
2019-09-24 10:15:37 +1200

    Add screening time to campaign schema and display it in the FE

--- client/src/components/campaign/CampaignDetails.js
+++ client/src/components/campaign/CampaignDetails.js
@@ -11,7 +11,7 @@ class CampaignDetails extends React.Component {
         <Tag style={styles.tag}>{ campaign.screenType }</Tag>
         <h2 style={styles.h2}><Icon type='compass' style={styles.icon} /> { campaign.cinemaName }</h2>
         <h2 style={styles.h2}><Icon type='calendar' style={styles.icon} /> { moment(campaign.screeningDate).format('MMMM Do YYYY') }</h2>
-        <h2 style={styles.h2}><Icon type='clock-circle' style={styles.icon} /> { moment(campaign.screeningDate).format('h:mm a') }</h2>
+        <h2 style={styles.h2}><Icon type='clock-circle' style={styles.icon} /> { campaign.screeningTime }</h2>
         <h2 style={styles.h2}><Icon type='dollar' style={styles.icon} /> ${ campaign.price }.00</h2>
         <h3 style={styles.h3}>Organiser: {campaign.creatorName} </h3>
       </div>
